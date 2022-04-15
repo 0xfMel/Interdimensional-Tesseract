bbeed845d9ad47f8a0db38c375bd609eca543eea
Mel ❤ <mel@femboydao.com>
2022-04-15 08:09:00 +0100

    Initial Commit

---
+++ src/main/java/ftm/_0xfmel/itdmtrct/ItdmTrct.java
@@ -0,0 +1,21 @@
+package ftm._0xfmel.itdmtrct;
+
+import ftm._0xfmel.itdmtrct.globals.ModGlobals;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ModGlobals.MOD_ID)
+public class ItdmTrct {
+    public ItdmTrct() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+}
---
+++ src/main/java/ftm/_0xfmel/itdmtrct/globals/ModGlobals.java
@@ -0,0 +1,5 @@
+package ftm._0xfmel.itdmtrct.globals;
+
+public class ModGlobals {
+    public static final String MOD_ID = "itdmtrct";
+}
